0d3525f45a746b1174e4b145abe944fb61c2ce57
saadivar <115896775+saadivar@users.noreply.github.com>
2023-12-03 18:39:51 +0100

    Delete req directory

--- req/srcs/hamid.sh
+++
@@ -1,6 +0,0 @@
-sudo docker-compose down -v
-sudo docker-compose down --rmi all
-
-sudo docker stop $(sudo docker ps -qa)
-sudo docker rm $(sudo docker ps -qa)
-sudo docker rmi $(sudo docker images )
